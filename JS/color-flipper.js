9954853d0c67b12de96d78a76b986a978ee237c7
Giulia Buttros <105403316+GiuliaButtros@users.noreply.github.com>
2023-05-26 19:52:52 -0300

    Início do script
    Add variáveis, funções e eventos

---
+++ JS/color-flipper.js
@@ -0,0 +1,31 @@
+const elementoBotaoNovaCor = document.getElementById('new-color-button'); // referente ao botão 
+
+const elementoCurrentColor = document.getElementById ('current-color'); //referente ao span
+
+const valoresHex = [
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+];
+
+function getRandomHexValue(){ // para selecionar valores hex aleatórios
+    
+}
+
+elementoBotaoNovaCor.addEventListener('click', function(){ // ao clicar, executa uma dada função
+    alert('ok');
+
+})
