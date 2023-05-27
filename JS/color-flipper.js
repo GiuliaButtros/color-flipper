77c13a6d664658782efdef6ce692f02469dbe18e
Giulia Buttros <105403316+GiuliaButtros@users.noreply.github.com>
2023-05-27 14:37:39 -0300

    JS: Adição de funções
    Função para selecionar valores hex aleatórios
    Função que cria uma string vazia para fazer loop n vezes (stringLength) e cada loop gera um valor Hex aleatório

--- JS/color-flipper.js
+++ JS/color-flipper.js
@@ -22,7 +22,29 @@ const valoresHex = [
 ];
 
 function getRandomHexValue(){ // para selecionar valores hex aleatórios
-    
+    // posição aleatória do index no array, retornar o elemento daquela posição do index
+
+    const randomIndexPosition = Math.floor(Math.random() * valoresHex.length); // multiplicar pela quantidade de valores (16 ou valoresHex.length caso mude por algum motivo)
+
+    // Envelopar com Math.floor pra ter certeza de que o resultado de Math.random será um número inteiro e não decimais 
+
+    const randomHexValue = valoresHex[randomIndexPosition]; // O que será retornado: Irá acessar os valores do array valoresHex e achar o ítem em randomIndexPosition
+
+    return randomHexValue;
+
+    //TODA ESSA FUNÇÃO ENTREGA 1 ÍTEM ALEATÓRIO
+}
+
+//Criando uma string vazia para fazer loop n vezes (stringLength) e cada loop gera um valor Hex aleatório, utililzando a função anterior para acrescentar à string atré formar a string completa
+
+function getRandomHexString(stringLength){ 
+    let hexString = ''; // variável string que será retornada
+
+    for (let i=0; i < stringLength; i++){
+        hexString += getRandomHexString();
+    }
+
+    return hexString;
 }
 
 elementoBotaoNovaCor.addEventListener('click', function(){ // ao clicar, executa uma dada função
