aca0886216c785b75948371b1407a1801ace4e42
Giulia Buttros <105403316+GiuliaButtros@users.noreply.github.com>
2023-05-31 19:37:50 -0300

    Adaptação do JS para o novo elemento
    Adicionadas cores complementares

--- JS/color-flipper.js
+++ JS/color-flipper.js
@@ -2,86 +2,54 @@ const elementoBotaoNovaCor = document.getElementById('new-color-button'); // ref
 
 const elementoCurrentColor = document.getElementById ('current-color'); //referente ao span
 
-const valoresHex = [
-    '0',
-    '1',
-    '2',
-    '3',
-    '4',
-    '5',
-    '6',
-    '7',
-    '8',
-    '9',
-    'A',
-    'B',
-    'C',
-    'D',
-    'E',
-    'F',
-];
+const elementoComplementar1 = document.getElementById ('color1');
 
-function getRandomHexValue(){ // para selecionar valores hex aleatórios
-    // posição aleatória do index no array, retornar o elemento daquela posição do index
+const elementoCurrentColorComp1 = document.getElementById ('current-comp1');
 
-    const randomIndexPosition = Math.floor(Math.random() * valoresHex.length); // multiplicar pela quantidade de valores (16 ou valoresHex.length caso mude por algum motivo)
+const elementoComplementar2 = document.getElementById ('color2');
 
-    // Envelopar com Math.floor pra ter certeza de que o resultado de Math.random será um número inteiro e não decimais 
+const elementoCurrentColorComp2 = document.getElementById ('current-comp2');
 
-    const randomHexValue = valoresHex[randomIndexPosition]; // O que será retornado: Irá acessar os valores do array valoresHex e achar o ítem em randomIndexPosition
+function randomCores(){
 
-    return randomHexValue;
+    let a1,a2,a3,b1,b2,b3,c1,c2,c3;
 
-    //TODA ESSA FUNÇÃO ENTREGA 1 ÍTEM ALEATÓRIO
+    a1 = randomComponente();
+    a2 = randomComponente();
+    a3 = randomComponente();
+
+    //Transladar os componentes para achar as cores complementares
+    b1 = a2;
+    b2 = a3;
+    b3 = a1; 
+
+    c1 = a3;
+    c2 = a1;
+    c3 = a2;
+
+    // Juntar os componentes para formar as 3 cores e retorná-las como array
+
+    return ["#"+a1+a2+a3,"#"+b1+b2+b3,"#"+c1+c2+c3]
 }
 
-//Criando uma string vazia para fazer loop n vezes (stringLength) e cada loop gera um valor Hex aleatório, utililzando a função anterior para acrescentar à string atré formar a string completa
-
-function getRandomHexString(stringLength){ 
-    let hexString = ''; // variável string que será retornada
-
-    for (let i=0; i < stringLength; i++){
-        hexString += getRandomHexValue();
-    }
-
-    return hexString;
+function randomComponente(){
+    hex = Math.floor(Math.random() * 256); // Gera um número decimal entre o 0 e o 255
+    return hex.toString(16).padStart(2,0); // Retorna o número convertido para hex como uma string
 }
 
 elementoBotaoNovaCor.addEventListener('click', function(){ // ao clicar, executa uma dada função
-    const randomHexString = '#' + getRandomHexString(6); // # para que o CSS reconheça o valor hex
+    
+    let arrayCores = randomCores();
+    let corA = arrayCores[0];
+    let corB = arrayCores[1];
+    let corC = arrayCores[2];
 
-    document.body.style.setProperty('background-color', randomHexString);
+    document.body.style.setProperty('background-color', corA);
+    elementoCurrentColor.textContent = corA;
 
-    elementoCurrentColor.textContent = randomHexString;
+    elementoComplementar1.style.setProperty('background-color', corB);
+    elementoCurrentColorComp1.textContent = corB;
+
+    elementoComplementar2.style.setProperty('background-color', corC);
+    elementoCurrentColorComp2.textContent = corC;
 });
-
-/*
-Os componentes RGB das cores A, B e C
-
-Exemplo: Se a1 = ff, a2 = 00 e a3 = 00 (ff0000), as complementares triadicas serão:
-
-b1=00 b2=ff b3=00 (00ff00) e c1=00 c2=00 c3=ff (0000ff)
-*/
-
-let a1, a2, a3, b1, b2, b3, c1, c2, c3;
-
-//FUNÇÃO para definir a1 a2 a3 aleatórios
-
-/*Transladar os componentes para achar as cores complementares
-
-b1 = a2;
-b2 = a3;
-b3 = a1; 
-
-c1 = a3;
-c2 = a1;
-c3 = a2;
-*/
-
-/* Juntar os componentes para formar as 3 cores
-
-let corA = a1+a2+a3;
-let corB = b1+b2+b3;
-let corC = c1+c2+c3;
-
-*/
--- README.md
+++ README.md
@@ -5,7 +5,11 @@
  Deploy:
 
  ## Estrutura
- []HTML
- []Display + h1 + span do hex code
- []CSS
- []JavaScript
+ [X]HTML;
+ [X]Display + h1 + span do hex code;
+ [X]CSS;
+ [X]JavaScript;
+    [X]Declaração variáveis;
+    [X]Função para declarar cores aleatórias + transladar componentes para encontrar cores complementares + retornar as 3 cores como array;
+    [X]Função para gerar número decimal e retornar o número convertido para hex como string;
+    [X]eventListener para disparar as funções e exibir o resultado
